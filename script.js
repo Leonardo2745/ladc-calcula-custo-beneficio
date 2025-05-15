4560ff7e14d700bf3c7e3a94539b8754cfde08ed
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-05-14 22:00:31 -0300

    mais updates

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("btnCalc").addEventListener("click", (event) => {
+event.preventDefault();
+  const precoUnitario = document.getElementById("preco-unitario").value;
+  const quantidade = document.getElementById("quantidade").value;
+  const desconto = document.getElementById("desconto").value;
+
+  const vlTotal = precoUnitario * quantidade;
+
+  const vlDesconto = vlTotal * (desconto / 100);
+
+  const resultado = vlTotal - vlDesconto;
+
+  const divResultado = document.getElementById("result");
+
+  divResultado.innerHTML = `<h3>${"O valor total é: R$" + resultado+",00 reais"}</h3>`;
+});
