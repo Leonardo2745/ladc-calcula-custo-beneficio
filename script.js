4f8e1273fc08eac9fed8893d8bd053f47fffcf58
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-05-15 21:59:43 -0300

    falta atualizar

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-document.getElementById("btnCalc").addEventListener("click", (event) => {
-event.preventDefault();
-  const precoUnitario = document.getElementById("preco-unitario").value;
-  const quantidade = document.getElementById("quantidade").value;
-  const desconto = document.getElementById("desconto").value;
+document.getElementById("btnCalc").addEventListener('click',(event)=>{
+    event.preventDefault();
 
-  const vlTotal = precoUnitario * quantidade;
+    const precoUnitario = document.getElementById("preco-unitario").value;
+    const quantidade = document.getElementById("quantidade").value;
+    const desconto = document.getElementById("desconto").value;
 
-  const vlDesconto = vlTotal * (desconto / 100);
+    const vlTotal = precoUnitario * quantidade;
 
-  const resultado = vlTotal - vlDesconto;
+    const vlDesconto = vlTotal * (desconto / 100);
 
-  const divResultado = document.getElementById("result");
+    const resultado = vlTotal - vlDesconto;
 
-  divResultado.innerHTML = `<h3>${"O valor total é: R$" + resultado+",00 reais"}</h3>`;
-});
+    const divResultado = document.getElementById("result");
+    
+    divResultado.innerHTML = `<h2 class='resultado'> O valor total é: R$${resultado} reais</h2>`
+})
